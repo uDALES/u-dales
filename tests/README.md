c26577b4d1fd869dd31b732dc973f6756e5ec27b
dmey <dmey@users.noreply.github.com>
2019-07-17 17:41:29 +0100

    init commit of comp tests

--- tests/README.md
+++ tests/README.md
@@ -2,3 +2,10 @@ This is a temporary placeholder for testing the uDALES software.
 The current test simply tests that the software does not crash at run-time.
 TODO: Add integration tests against ref datasets.
 TODO: Add multiple test cases.
+
+Requirements Python 3.5 or above
+
+```
+pip install -r requirements.txt
+python3 tests/run_tests.py master dmey/external-libs Release
+```
---
+++ tests/scripts/build_model.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+# uDALES (https://github.com/uDALES/u-dales).
+# Copyright (C) 2019 D. Meyer.
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Build uDALES with CMake.
+
+"""
+
+import os
+import shutil
+import subprocess
+from pathlib import Path
+
+
+def build_from_branch(branch_name: str, path_to_proj_dir: Path, build_type: str, clean_build_dir=False) -> str:
+    subprocess.run(['git', 'checkout', branch_name])
+    path_to_build_dir = path_to_proj_dir / 'build' / branch_name.replace('/', '_') # Common branch names use / as user separator.
+    build(path_to_proj_dir, path_to_build_dir, build_type, clean_build_dir=False)
+    return path_to_build_dir
+
+def build(path_to_proj_dir: Path, path_to_build_dir: Path, build_type: str, clean_build_dir=False) -> None:
+    if clean_build_dir:
+        shutil.rmtree(path_to_build_dir, ignore_errors=True)
+    if not path_to_build_dir.is_dir():
+        path_to_build_dir.mkdir(parents=True)
+
+    subprocess.run(['cmake', f'-DCMAKE_BUILD_TYPE={build_type}', path_to_proj_dir, '-LA'], cwd=path_to_build_dir)
+
+    cpu_count = str(os.cpu_count())
+    subprocess.run(['cmake', '--build', '.', '--', '-j', cpu_count], cwd=path_to_build_dir)
+    return None
+
+if __name__ == "__main__":
+    pass
