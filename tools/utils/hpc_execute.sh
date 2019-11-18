a48674fed29b7d547539eef979faeda768c6b02f
Birgit Sutzl <bss116@ic.ac.uk>
2019-11-18 14:05:45 +0100

    read in config.sh for variables call now from anywhere with path to experiment directory

--- tools/utils/hpc_execute.sh
+++ tools/utils/hpc_execute.sh
@@ -1,46 +1,53 @@
 #!/bin/bash
 
-## automatically set the experiment number via path
-cdir=$(pwd)
-exp="${cdir: -3}"
-echo "experiment number: ${exp}"
+set -e
 
-# some default parameters, changes can be made here
-# This should really be an arg and not harcoded!
-executable=${DA_TOPDIR}/u-dales/build/release/u-dales
-utilspath=${DA_TOPDIR}/u-dales/tools/utils
-ncpu=12
+if (( $# < 1 ))
+then
+ echo "The experiment directory must be set."
+ exit
+fi
 
-## HPC specific
-nnode=5
-walltime="24:00:00"
-mem="20gb"
-
-## directories
-outdir=${EPHEMERAL}/${exp}
+## go to experiment directory
+pushd $1
 inputdir=$(pwd)
 
+## set experiment number via path
+exp="${inputdir: -3}"
+
+echo "experiment number: $exp"
+
+## read in additional variables
+if [ -f config.sh ]; then
+    source config.sh
+fi
+
+## set the output directory
+outdir=$EPHEMERAL/$exp
+
+echo "writing job.$exp."
+
 ## write new job.exp file for HPC
-echo "#PBS -l walltime=${walltime}" > job.${exp}
-echo "#PBS -l select=${nnode}:ncpus=${ncpu}:mem=${mem}" >> job.${exp}
+echo "#PBS -l walltime=${WALLTIME}" > job.$exp
+echo "#PBS -l select=${NNODE}:ncpus=${NCPU}:mem=${MEM}" >> job.$exp
 
 ## load modules required for the execution
-echo "module load intel-suite/2017.6 mpi/intel-2018 cmake/3.14.0 git/2.14.3" >> job.${exp}
+echo "module load intel-suite/2017.6 mpi/intel-2018 cmake/3.14.0 git/2.14.3" >> job.$exp
 
 ## copy files to execution and output directory
-echo "mkdir ${outdir}" >> job.${exp}
-echo "cp ${inputdir}/* ${outdir}" >> job.${exp}
+echo "mkdir -p $outdir" >> job.$exp
+echo "cp $inputdir/* $outdir" >> job.$exp
 
 ## go to execution and output directory
-echo "cd ${outdir}" >> job.${exp}
+echo "pushd $outdir" >> job.$exp
 
 ## execute program with mpi
-echo "mpiexec -n $(( ${ncpu} * ${nnode} )) ${executable} ${outdir}/namoptions.${exp} > ${outdir}/output.${exp} 2>&1" >> job.${exp}
+echo "mpiexec -n $(( $NCPU * $NNODE )) $DA_BUILD $outdir/namoptions.$exp > $outdir/output.$exp 2>&1" >> job.$exp
 
-## merge output files from several cpus to one file
-echo "${utilspath}/da_concatenate.sh ${outdir} " >> job.${exp}
+## merge output files from cores to one file
+echo "$DA_UTILSDIR/da_concatenate.sh $outdir " >> job.$exp
 
 ## submit job.exp file to queue
-qsub job.${exp}
+qsub job.$exp
 
-echo "job.${exp} submitted."
+echo "job.$exp submitted."
