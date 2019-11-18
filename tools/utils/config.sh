e382c3e38bbdfc7c1da7b21c5585421327cf26d4
Birgit Sutzl <bss116@ic.ac.uk>
2019-11-18 14:42:16 +0100

    change variable name

--- tools/utils/config.sh
+++ tools/utils/config.sh
@@ -6,7 +6,7 @@ DA_BUILD=$DA_TOPDIR/u-dales/build/release/u-dales # Executable
 NCPU=2 # Number of CPUs to use for a simulation
 
 # local only
-OUTPUT=$DA_TOPDIR/outputs # Output top-level directory
+DA_WORKDIR=$DA_TOPDIR/outputs # Output top-level directory
 
 # HPC only
 NNODE=5 # Number of nodes to use for a simulation
