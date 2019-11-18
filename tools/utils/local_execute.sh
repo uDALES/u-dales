796c58a146ee21580b2a713ca2062e17d61c8af7
Birgit Sutzl <bss116@ic.ac.uk>
2019-11-18 14:04:27 +0100

    read in config.sh for variables call now with path to experiment directory

--- tools/utils/local_execute.sh
+++ tools/utils/local_execute.sh
@@ -2,55 +2,63 @@
 
 set -e
 
-
 if (( $# < 1 ))
 then
- echo "The experiment ID must be set."
- exit
+    echo "The experiment directory must be set."
+    exit
 fi
 
-exp=$(printf "%03.0f" $1)    #pad argument 1 (target simulation) with zeros
+## go to experiment directory
+pushd $1
+inputdir=$(pwd)
 
+## set experiment number via path
+exp="${inputdir: -3}"
+
+echo "experiment number: $exp"
+
+## read in additional variables
+if [ -f config.sh ]; then
+    source config.sh
+fi
+
+## check if required variables are set
+## or set default if not
 if [ -z $NCPU ]; then
-  NCPU=1
+    NCPU=1
 fi;
 if [ -z $DA_WORKDIR ]; then
-  echo "DA_WORKDIR must be set"
-  exit
-fi;
-if [ -z $DA_EXPDIR ]; then
-  echo "DA_EXPDIR must be set"
-  exit
-fi;
-if [ -z $DA_UTILSDIR ]; then
-  echo "DA_UTILSDIR must be set"
-  exit
+    echo "Output top-level directory DA_WORKDIR must be set"
+    exit
 fi;
 if [ -z $DA_BUILD ]; then
-  echo "DA_BUILD must be set"
-  exit
+    echo "Executable DA_BUILD must be set"
+    exit
+fi;
+if [ -z $DA_UTILSDIR ]; then
+    echo "Utils directory DA_UTILSDIR must be set"
+    exit
 fi;
 
-## automatically set the experiment number via path
-inputdir=${DA_EXPDIR}/${exp}
-echo "experiment number: ${exp}"
-outdir=${DA_WORKDIR}/${exp}
+## set the experiment output directory
+outdir=$DA_WORKDIR/$exp
 
-echo "starting job.${exp}."
+echo "starting job.$exp."
 
 ## copy files to output directory
 mkdir -p $outdir
-cp ${inputdir}/* $outdir
+cp ./* $outdir
 
 ## go to execution and output directory
 pushd $outdir
 
 ## execute program with mpi
-mpiexec -n ${NCPU} ${DA_BUILD} namoptions.${exp} > output.${exp} 2>&1
+mpiexec -n $NCPU $DA_BUILD namoptions.$exp > output.$exp 2>&1
 
 ## merge output files from cores to one file
-${DA_UTILSDIR}/da_concatenate.sh ${outdir}
+export LOCAL_EXECUTE=1
+$DA_UTILSDIR/da_concatenate.sh $outdir
 
 popd
 
-echo "job.${exp} done."
+echo "job.$exp done."
