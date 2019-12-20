5dbe3fdffd5335db56f83bd267417a1394843039
dmey <dmey@users.noreply.github.com>
2019-12-19 23:31:27 -0500

    add wrapper script [wip] [skip ci]

---
+++ tools/utils/hpc_build.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/env bash
+set -e
+
+# Usage: hpc_build.sh [icl, archer, cca, common] [debug, release]
+
+if [ ! -d src ]; then
+    echo "Please run this script from the project folder"
+    exit 1
+fi
+
+
+capitalize() {
+    echo $* | sed -e "s/\b\(.\)/\u\1/g"
+}
+
+NPROC=2
+system=$1
+build_type=$2
+
+
+if [ $system == "icl" ]
+then
+    module load cmake/3.14.0 git/2.14.3 intel-suite/2017.6 mpi/intel-2018
+    FC=mpiifort
+    NETCDF_DIR=/apps/netcdf/4.4.1-c
+    NETCDF_FORTRAN_DIR=/apps/netcdf/4.4.4-fortran
+
+elif [ $system == "archer" ]
+then
+    echo "Configuration not currently available"
+
+elif [ $system == "cca" ]
+then
+    module load git cmake
+    FC=ftn
+    NETCDF_DIR=$NETCDF_DIR
+    NETCDF_FORTRAN_DIR=$NETCDF_DIR
+
+elif [ $system == "common" ]
+then
+    FC=
+    NETCDF_DIR=
+    NETCDF_FORTRAN_DIR=
+
+else
+    echo "This configuration is not avalable"
+    exit 1
+fi
+
+# Configure and Build
+path_to_build_dir="$(pwd)/build/$build_type"
+mkdir -p $path_to_build_dir
+pushd $path_to_build_dir
+cmake_build_type="$(capitalize $build_type)"
+FC=$FC cmake -DNETCDF_DIR=$NETCDF_DIR \
+             -DNETCDF_FORTRAN_DIR=$NETCDF_FORTRAN_DIR \
+             -DCMAKE_BUILD_TYPE=cmake_build_type \
+              ../../ 2>&1 | tee -a $path_to_build_dir/config.log
+make -j$NPROC 2>&1 | tee -a $path_to_build_dir/build.log
+popd
