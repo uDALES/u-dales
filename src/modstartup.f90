29d41754d7e878f5ad1df2a336ce4d8a263a8df6
dmey <dmey@users.noreply.github.com>
2021-03-18 12:57:11 +0100

    fix headers

--- src/modstartup.f90
+++ src/modstartup.f90
@@ -13,6 +13,7 @@
 !!  \todo documentation
 !!  \par Revision list
 !
+!  This file is part of DALES.
 !
 ! DALES is free software; you can redistribute it and/or modify
 ! it under the terms of the GNU General Public License as published by
--- src/modsubgriddata.f90
+++ src/modsubgriddata.f90
@@ -12,6 +12,8 @@
 !!  \par Revision list
 !!  \todo Documentation
 !
+!  This file is part of DALES.
+!
 ! DALES is free software; you can redistribute it and/or modify
 ! it under the terms of the GNU General Public License as published by
 ! the Free Software Foundation; either version 3 of the License, or
