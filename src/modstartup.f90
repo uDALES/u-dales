b0f67e811b6464928c7f4e06f8cfa033a749f278
samoliverowens <sam.owens18@imperial.ac.uk>
2023-05-10 12:01:46 +0100

    Correct warmstart EB interaction.

--- src/modstartup.f90
+++ src/modstartup.f90
@@ -1758,6 +1758,8 @@ module modstartup
       ntimee = nint(timee/dtmax)
       tnextrestart = btime + trestart
       tnextfielddump = btime + tfielddump
+      tEB = btime
+      tnextEB = btime + dtEB
       deallocate (height, th0av)
 
       !    call boundary
