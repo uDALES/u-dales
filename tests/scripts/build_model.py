2e767cdf36c10573927caf8eeee2ec31fe77446c
samoliverowens <56388937+samoliverowens@users.noreply.github.com>
2023-05-31 11:46:21 +0100

    Try including submodules when running tests.

--- tests/scripts/build_model.py
+++ tests/scripts/build_model.py
@@ -29,6 +29,7 @@ from pathlib import Path
 
 def build_from_branch(branch_name: str, path_to_proj_dir: Path, build_type: str, clean_build_dir=False, skip_build=False) -> str:
     subprocess.run(['git', 'checkout', branch_name])
+    subprocess.run(['git', 'submodule', 'update', '--init'])
     # Common branch names use / as user separator.
     path_to_build_dir = path_to_proj_dir / 'build' / branch_name.replace('/', '_')
     if not skip_build:
