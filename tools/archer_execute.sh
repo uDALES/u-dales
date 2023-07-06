217b6740614c08accd222d8ab0a40e1257e33e80
samoliverowens <56388937+samoliverowens@users.noreply.github.com>
2023-07-06 16:01:38 +0100

    Build and execute changes.

--- tools/archer_execute.sh
+++ tools/archer_execute.sh
@@ -46,7 +46,6 @@ echo "#SBATCH --partition=standard" >> job.$exp.slurm
 echo "#SBATCH --qos=${QOS}" >> job.$exp.slurm
 echo "module load epcc-job-env" >> job.$exp.slurm
 echo "export OMP_NUM_THREADS=1" >> job.$exp.slurm
-echo "export SRUN_CPUS_PER_TASK=$SLURM_CPUS_PER_TASK" >> job.$exp.slurm
 echo "srun --distribution=block:block --hint=nomultithread ./u-dales $outdir/namoptions.$exp > $outdir/output.$exp 2>&1" >> job.$exp.slurm
 
 ## gather output files from cores in a single file
