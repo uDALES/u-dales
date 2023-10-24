ab5b80552dd14268aee433ea48bc739327110389
samoliverowens <56388937+samoliverowens@users.noreply.github.com>
2023-10-24 16:25:09 +0100

    Merge master into trees branch. (#165)
    * Allow MPI_BCAST to be used for multiple data types.
    
    Necessary when gfortran version > 9. In future should probably be handled using interfaces.
    
    * Use latest gfortran version in tests and fix debug compilation. (#150)

--- .github/scripts/install_deps.sh
+++ .github/scripts/install_deps.sh
@@ -6,7 +6,6 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     sudo apt update
     sudo apt install gfortran libopenmpi-dev openmpi-bin libnetcdf-dev libnetcdff-dev graphviz
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-    brew install netcdf open-mpi graphviz
-    brew unlink gcc
-    brew link gcc@9
+    brew install netcdf netcdf-fortran open-mpi graphviz
+    brew link gcc
 fi
