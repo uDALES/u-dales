002d653e575a6a8a5e17570d05e325f1f8008ee8
samoliverowens <56388937+samoliverowens@users.noreply.github.com>
2024-02-15 16:56:50 +0000

    Bug fixes (MPI) for outflow rate forcing.

--- src/modforces.f90
+++ src/modforces.f90
@@ -350,7 +350,7 @@ module modforces
 
     use modglobal, only : ib,ie,jb,je,ih,jh,kb,ke,kh,dzf,dxf,dy,zh,dt,rk3step,&
                           uflowrate,vflowrate,linoutflow,&
-                          luoutflowr,lvoutflowr,luvolflowr,lvvolflowr,ierank,jerank
+                          luoutflowr,lvoutflowr,luvolflowr,lvvolflowr
     use modfields, only : um,up,vm,vp,uout,uouttot,udef,vout,vouttot,vdef,&
                           uoutarea,voutarea,fluidvol,IIu,IIv,IIus,IIvs
     use modmpi,    only : myid,comm3d,mpierr,nprocs,MY_REAL,sumy_ibm,sumx_ibm,avexy_ibm
@@ -370,34 +370,31 @@ module modforces
       rk3coef = dt / (4. - dble(rk3step))
       rk3coefi = 1 / rk3coef
 
-      if (ierank) then
-         udef = 0.
-         uout = 0.
-         uoutflow = 0.
-         uoutold = 0.
+      ! Assumes ie=itot
+      udef = 0.
+      uout = 0.
+      uoutflow = 0.
+      uoutold = 0.
 
-         ! integrate u fixed at outlet ie along y
-         call sumy_ibm(uout,up(ie,jb:je,kb:ke)*dy,ie,ie,jb,je,kb,ke,IIu(ie,jb:je,kb:ke))  ! u tendency at previous time step
-         call sumy_ibm(uoutold,um(ie,jb:je,kb:ke)*dy,ie,ie,jb,je,kb,ke,IIu(ie,jb:je,kb:ke))  ! u at previous time step
+      ! integrate u fixed at outlet ie along y
+      call sumy_ibm(uout,up(ie,jb:je,kb:ke)*dy,ie,ie,jb,je,kb,ke,IIu(ie,jb:je,kb:ke))  ! u tendency at previous time step
+      call sumy_ibm(uoutold,um(ie,jb:je,kb:ke)*dy,ie,ie,jb,je,kb,ke,IIu(ie,jb:je,kb:ke))  ! u at previous time step
 
-         ! integrate u in z
-         do k=kb,ke
-            uout(k) = rk3coef*uout(k)*dzf(k)
-            uoutold(k) = uoutold(k)*dzf(k)
-         end do
+      ! integrate u in z
+      do k=kb,ke
+         uout(k) = rk3coef*uout(k)*dzf(k)
+         uoutold(k) = uoutold(k)*dzf(k)
+      end do
 
-         uoutflow = sum(uout(kb:ke))
-         uflowrateold = sum(uoutold(kb:ke))
+      uoutflow = sum(uout(kb:ke))
+      uflowrateold = sum(uoutold(kb:ke))
 
-         ! average over outflow area
-         uoutflow = uoutflow/uoutarea
-         uflowrateold = uflowrateold/uoutarea
+      ! average over outflow area
+      uoutflow = uoutflow/uoutarea
+      uflowrateold = uflowrateold/uoutarea
 
-         ! flow correction to match outflow rate
-         udef = uflowrate - (uoutflow + uflowrateold)
-      end if
-
-      call MPI_BCAST(udef, 1, MY_REAL, nprocs-1, comm3d, mpierr)
+      ! flow correction to match outflow rate
+      udef = uflowrate - (uoutflow + uflowrateold)
 
       do k = kb,ke
         do j = jb,je
@@ -469,40 +466,37 @@ module modforces
       rk3coef = dt / (4. - dble(rk3step))
       rk3coefi = 1 / rk3coef
 
-      if (jerank) then
-         vdef = 0.
-         vout = 0.
-         voutflow = 0.
-         voutold = 0.
+      ! Assumes je=jtot
+      vdef = 0.
+      vout = 0.
+      voutflow = 0.
+      voutold = 0.
 
-         ! integrate v fixed at outlet je along x
-         ! if (myid==nprocs-1) then
-         !    do k=kb,ke
-         !       vout(k) = sum(vp(ib:ie,je,k)*IIv(ib:ie,je,k)*dxf(ib:ie))  ! v tendency at previous time step
-         !       voutold(k) = sum(vm(ib:ie,je,k)*IIv(ib:ie,je,k)*dxf(ib:ie))  ! v at previous time step
-         !    end do
-         ! end if
-         call sumy_ibm(vout,vp(ib:je,je,kb:ke)*dxf(1),ib,ie,je,je,kb,ke,IIv(ib:ie,je,kb:ke))  ! v tendency at previous time step
-         call sumy_ibm(voutold,vm(ib:ie,je,kb:ke)*dxf(1),ib,ie,je,je,kb,ke,IIv(ib:ie,je,kb:ke))  ! v at previous time step
+      ! integrate v fixed at outlet je along x
+      ! if (myid==nprocs-1) then
+      !    do k=kb,ke
+      !       vout(k) = sum(vp(ib:ie,je,k)*IIv(ib:ie,je,k)*dxf(ib:ie))  ! v tendency at previous time step
+      !       voutold(k) = sum(vm(ib:ie,je,k)*IIv(ib:ie,je,k)*dxf(ib:ie))  ! v at previous time step
+      !    end do
+      ! end if
+      call sumy_ibm(vout,vp(ib:je,je,kb:ke)*dxf(1),ib,ie,je,je,kb,ke,IIv(ib:ie,je,kb:ke))  ! v tendency at previous time step
+      call sumy_ibm(voutold,vm(ib:ie,je,kb:ke)*dxf(1),ib,ie,je,je,kb,ke,IIv(ib:ie,je,kb:ke))  ! v at previous time step
 
-         ! integrate v in z
-         do k=kb,ke
-            vout(k) = rk3coef*vout(k)*dzf(k)
-            voutold(k) = voutold(k)*dzf(k)
-         end do
+      ! integrate v in z
+      do k=kb,ke
+         vout(k) = rk3coef*vout(k)*dzf(k)
+         voutold(k) = voutold(k)*dzf(k)
+      end do
 
-         voutflow = sum(vout(kb:ke))
-         vflowrateold = sum(voutold(kb:ke))
+      voutflow = sum(vout(kb:ke))
+      vflowrateold = sum(voutold(kb:ke))
 
-         ! average over outflow area
-         voutflow = voutflow/voutarea
-         vflowrateold = vflowrateold/voutarea
+      ! average over outflow area
+      voutflow = voutflow/voutarea
+      vflowrateold = vflowrateold/voutarea
 
-         ! flow correction to match outflow rate
-         vdef = vflowrate - (voutflow + vflowrateold)
-      end if
-
-      call MPI_BCAST(vdef, 1, MY_REAL, nprocs-1, comm3d, mpierr)
+      ! flow correction to match outflow rate
+      vdef = vflowrate - (voutflow + vflowrateold)
 
       do k = kb,ke
         do j = jb,je
@@ -572,7 +566,7 @@ module modforces
 
     use modglobal, only   : ib,ie,jb,je,kb,ke,dy,dzf,ierank
     use modfields, only   : IIc
-    use modmpi, only      : myid,comm3d,mpierr,nprocs,MY_REAL,sumy_ibm
+    use modmpi, only      : sumy_ibm
 
     implicit none
     real, intent(out)       :: area
@@ -581,10 +575,8 @@ module modforces
 
     sumy = 0.
     ! integrate fluid area at outflow plane in y
-    if (ierank) then
-      call sumy_ibm(sumy,IIc(ie,jb:je,kb:ke)*dy,ie,ie,jb,je,kb,ke,IIc(ie,jb:je,kb:ke))
-    end if
-    call MPI_BCAST(sumy,ke-kb+1,MY_REAL,nprocs-1,comm3d,mpierr)
+    ! Assumes ie=itot
+    call sumy_ibm(sumy,IIc(ie,jb:je,kb:ke)*dy,ie,ie,jb,je,kb,ke,IIc(ie,jb:je,kb:ke))
 
     ! integrate fluid area at outflow plane in z
     do k=kb,ke
@@ -599,7 +591,7 @@ module modforces
 
     use modglobal, only : ib,ie,jb,je,kb,ke,dxf,dzf,jerank
     use modfields, only : IIc
-    use modmpi,    only : myid,comm3d,mpierr,nprocs,MY_REAL,sumx_ibm
+    use modmpi,    only : sumx_ibm
 
     implicit none
     real, intent(out)       :: area
@@ -608,11 +600,8 @@ module modforces
 
     sumx = 0.
     ! integrate fluid area at outflow plane in x
-    if (jerank) then
-      call sumx_ibm(sumx,IIc(ib:ie,je,kb:ke)*dxf(1),ib,ie,je,je,kb,ke,IIc(ib:ie,je,kb:ke))
-    end if
-
-    call MPI_BCAST(sumx,ke-kb+1,MY_REAL,nprocs-1,comm3d,mpierr)
+    ! Assumes je=jtot
+    call sumx_ibm(sumx,IIc(ib:ie,je,kb:ke)*dxf(1),ib,ie,je,je,kb,ke,IIc(ib:ie,je,kb:ke))
 
     ! integrate fluid area at outflow plane in z
     do k=kb,ke
