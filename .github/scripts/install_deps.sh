78f3e823ffa0972582e7cb5639d8124d4eb244c2
samoliverowens <56388937+samoliverowens@users.noreply.github.com>
2024-08-26 13:28:57 +0100

    Update install_deps.sh

--- .github/scripts/install_deps.sh
+++ .github/scripts/install_deps.sh
@@ -6,6 +6,6 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     sudo apt update
     sudo apt install gfortran libopenmpi-dev openmpi-bin libnetcdf-dev libnetcdff-dev graphviz libfftw3-dev
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-    brew install netcdf netcdf-fortran open-mpi graphviz
+    brew install netcdf netcdf-fortran mpich graphviz fftw
     brew link gcc
 fi
