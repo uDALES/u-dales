4fbbac1a8f65328f73917ffab0429dccdb8133e5
Dipanjan Majumdar <dipanjanmajumdar100@gmail.com>
2024-08-28 17:11:18 +0100

    Significant update to copy_inputs

--- tools/copy_inputs.sh
+++ tools/copy_inputs.sh
@@ -19,30 +19,47 @@
 
 # examples:
 # new sim: "copy_inputs.sh 1 2" or "copy_inputs.sh 1 2 c"
-# continue with old sim: "copy_inputs.sh 1 1 w"
 # continue with new sim  "copy_inputs.sh 1 2 w"
 
 set -e
 
-if [ -z $DA_EXPDIR_SRC ]; then
-  DA_EXPDIR_SRC=$DA_EXPDIR
-fi
-
-if [ -z $DA_WORKDIR_SRC ]; then
-  DA_WORKDIR_SRC=$DA_WORKDIR
-fi
-
 if (( $# < 2 )) ; then
-  echo "usage: `basename $0` sim#1 sim#2 (start)"
-  echo "prepares a new simulation with DALES"
-  echo "   sim#1: number of simulation upon which the new one is based"
-  echo "   sim#2: number of new simulation"
+  echo "usage: `basename $0` sim#1_local_path sim#2 (start)"
+  echo "prepares a new simulation based on an old one with uDALES"
+  echo "   sim#1: local path of source case upon which the new one will be based on"
+  echo "   sim#2: three digit integer case number of the new simulation"
   echo "   start (optional): (c)old- or (w)arm-start, c is default"
   echo "... execution terminated"
   exit 0
 fi
 
-src=$(printf "%03.0f" $1)    # pad argument 1 (origin) with zeros
+# go to experiment directory
+pushd $1
+	inputdir=$(pwd)
+
+	## set source experiment number via path of the source directory
+	src="${inputdir: -3}"
+
+	## read in additional variables
+	if [ -f config.sh ]; then
+   	 source config.sh
+	else
+	 echo "config.sh must be set inside $1"
+     exit 0
+	fi
+
+	## check if required variables are set
+	if [ -z $DA_EXPDIR ]; then
+		echo "Experiment directory DA_EXPDIR must be set $1/config.sh"
+		exit
+	fi;
+  if [ -z $DA_WORKDIR ]; then
+		echo "Experiment directory DA_WORKDIR must be set $1/config.sh"
+		exit
+	fi;
+
+popd
+
 tar=$(printf "%03.0f" $2)    # pad argument 2 (target simulation) with zeros
 start=${3:-c}                # default argument 3 to c (coldstart) if not provided
 case=1                       # default value 1 (target folder does not exist or overwrite, 2=only copy files that don't exist, 3=interactive)
@@ -74,92 +91,113 @@ else
 fi
 
 # test if original simulation exists
-if [ ! -d $DA_EXPDIR_SRC/$src ]; then
+if [ ! -d $DA_EXPDIR/$src ]; then
   echo "original simulation $src does not exist in $DA_EXPDIR"
   echo "exit"
   exit 1
 fi
 
 # list of files to copy
-declare -a tocopy=("/namoptions." "/lscale.inp." "/prof.inp." "/scalar.inp." "/purifs.inp." "/trees.inp." "/scals.inp." "/lad.inp." "/facetarea.inp." "/facets.inp." "/netsw.inp." "/svf.inp." "/Tfacinit.inp." "/vf.nc.inp." "/factypes.inp.")
+declare -a tocopy=("/namoptions." "/lscale.inp." "/prof.inp." "/scalar.inp." "/purifs.inp." "/trees.inp." "/scals.inp." "/lad.inp." "/facetarea.inp." "/facets.inp." "/facets_unused." "/netsw.inp." "/svf.inp." "/Tfacinit.inp." "/vf.nc.inp." "/factypes.inp.")
 
 # copy and rename files
 case $case in
   1)  #target folder didn't exist or just overwrite all files
     for i in "${tocopy[@]}"
       do
-        cp $DA_EXPDIR_SRC/$src$i$src $DA_EXPDIR/$tar$i$tar 2>/dev/null || :
+        cp $DA_EXPDIR/$src$i$src $DA_EXPDIR/$tar$i$tar 2>/dev/null || :
         #echo $i
       done
   ;;
   2)  #don't overwrite files in target folder #cp -n does not overwrite an existing file
     for i in "${tocopy[@]}"
       do
-        cp -n $DA_EXPDIR_SRC/$src$i$src $DA_EXPDIR/$tar$i$tar 2>/dev/null || :
+        cp -n $DA_EXPDIR/$src$i$src $DA_EXPDIR/$tar$i$tar 2>/dev/null || :
         #echo $i
       done
   ;;
   3)  #ask for every file if to overwrite or not
     for i in "${tocopy[@]}"
       do
-        cp -i $DA_EXPDIR_SRC/$src$i$src $DA_EXPDIR/$tar$i$tar 2>/dev/null || :
+        cp -i $DA_EXPDIR/$src$i$src $DA_EXPDIR/$tar$i$tar 2>/dev/null || :
         #echo $i
       done
   ;;
 esac
 
+
+####### modified function for sed -i
+function sedi { if [[ "$OSTYPE" == "darwin"* ]]; then
+        		sed -i '' "$1" "$2"
+		elif [[ "$OSTYPE" == "linux"* ]]; then
+        		sed -i "$1" "$2"
+		fi;}
+
+function update_namoptions() {
+    local varname=$1
+    local value=$2
+    local sectionname=$3
+    if grep -w -q "$varname" "$DA_EXPDIR/$tar/namoptions.$tar"; then
+        sedi "/$varname /s/.*/$varname       = $value/g" "$DA_EXPDIR/$tar/namoptions.$tar"
+    else
+        sedi "/$sectionname/a\\
+$varname = $value" "$DA_EXPDIR/$tar/namoptions.$tar"
+    fi
+}
+
 # change simulation number in namoptions
-sed -i.bak -e "/iexpnr/s/.*/iexpnr       = $tar/g" $DA_EXPDIR/$tar"/namoptions."$tar
-rm $DA_EXPDIR/$tar"/namoptions."$tar".bak"
+update_namoptions 'iexpnr' $tar '&RUN'
 
 # coldstart: set warmstart to false in namoptions
 if [ $start == "c" ]; then
   sed -i.bak -e '/lwarmstart/s/.*/lwarmstart =  .false./g' $DA_EXPDIR/$tar"/namoptions."$tar
   rm $DA_EXPDIR/$tar"/namoptions."$tar".bak"
-# warmstart: link newest warmstart files
 
+# warmstart: link newest warmstart files
 elif [ $start == "w" ]; then
   # momentum warmstart files (minimum requirement for warmstarts)
-  startfilen=$(ls -t $DA_WORKDIR_SRC/$src"/initd"* | head -1)
+  startfilen=$(ls -t $DA_WORKDIR/$src"/initd"* | head -1)
   if [ -z "$startfilen" ]; then
-    echo "no restart files found in $DA_WORKDIR_SRC/$src"
+    echo "no restart files found in $DA_WORKDIR/$src"
     echo "exit"
     exit 1
   else
     # create links
     startfilen=${startfilen##*/}  # retain the part after the last slash
     startfilen=${startfilen%_*_*}   # retain the part before the underscore
-    ln -s $DA_WORKDIR_SRC/$src"/"*$startfilen* $DA_EXPDIR/$tar/
+    ln -s $DA_WORKDIR/$src"/"*$startfilen* $DA_EXPDIR/$tar/
   fi
 
+  update_namoptions 'lwarmstart' '.true.' '&RUN'
+  update_namoptions 'startfile' $startfilen\_xxx_xxx.$tar '&RUN'
+
   # scalar warmstart files
-  scalarfilen=$(ls -t $DA_WORKDIR_SRC/$src"/inits"* | head -1)
-  if [ -z "$scalarfilen" ]; then
-    echo "Info: no scalar restart files found in $DA_WORKDIR_SRC/$src."
-  else
+  scalarwarmstart=$DA_WORKDIR/$src"/inits"*
+  if compgen -G "$scalarwarmstart" > /dev/null; then
+
+    scalarfilen=$(ls -t $DA_WORKDIR/$src"/inits"* | head -1)
+      
     # create links
     scalarfilen=${scalarfilen##*/}  # retain the part after the last slash
     scalarfilen=${scalarfilen%_*_*}   # retain the part before the underscore
-    ln -s $DA_WORKDIR_SRC/$src"/"*$scalarfilen* $DA_EXPDIR/$tar/
+    ln -s $DA_WORKDIR/$src"/"*$scalarfilen* $DA_EXPDIR/$tar/
+
+    update_namoptions 'lreadscal' '.true.' '&SCALARS'
+
+    else
+      echo "Info: no scalar restart files found in $DA_WORKDIR/$src."
   fi
 
   # rename links
   for f in $DA_EXPDIR/$tar/*.$src; do
     mv $f "${f%.$src}.$tar"
   done
-  echo "Creating links to warmstart files in $DA_WORKDIR_SRC/$src."
-
-  sed -i.bak -e '/lwarmstart/s/.*/lwarmstart   = .true./g' $DA_EXPDIR/$tar"/namoptions."$tar # set warmstart to true in namoptions
-  sed -i.bak -e "/startfile/s/.*/startfile    = '$startfilen\_xxx_xxx.$tar'/g" $DA_EXPDIR/$tar"/namoptions."$tar # change startfile to newest restartfiles
-  rm $DA_EXPDIR/$tar"/namoptions."$tar".bak"
-  # Note that this only works if lwarmstart and startfile are defined in namoptions!
-  # TODO: If they are not defined under RUN section in namoptions, add them!
-  echo "Warning! Check that lwarmstart = .true. and startfile = '${startfilen}_xxx_xxx.${tar}' in namoptions. If not, add them to the RUN section."
+  echo "Creating links to warmstart files in $DA_WORKDIR/$src."
 
 fi
 
 # link any available driver files (without renaming)
-driverfiles=$DA_WORKDIR_SRC/$src"/"?"driver_"*
+driverfiles=$DA_WORKDIR/$src"/"?"driver_"*
 for dfile in $driverfiles ; do
     if [ -f $dfile ]; then
       ln -s $dfile $DA_EXPDIR/$tar
@@ -170,22 +208,31 @@ if [ -f $dfile ]; then
 fi
 
 # copy config script for execution
-config_script=$DA_EXPDIR_SRC/$src/config.sh
+config_script=$DA_EXPDIR/$src/config.sh
 if [ -f $config_script ] ; then
   cp $config_script $DA_EXPDIR/$tar
-else
-  echo 'Skipping config file (no config.sh file found).'
+fi
+
+# copy STL for execution
+stlfile=$(find $DA_EXPDIR/$src -iname "*.stl")
+if [ -f "$stlfile" ] ; then
+  cp "$stlfile" $DA_EXPDIR/$tar
 fi
 
 # copy *.txt files for IBM implementation
-  cp $DA_EXPDIR_SRC/$src/*.txt $DA_EXPDIR/$tar
+IBM_files=$DA_EXPDIR/$src/*.txt
+for ibmfile in $IBM_files ; do
+  if [ -f $ibmfile ]; then
+    cp $ibmfile $DA_EXPDIR/$tar
+  fi
+done
 
 # copy any available scalar source files
-  scalarsourcefiles=$DA_EXPDIR_SRC/$src/"scalarsource"*
-  for sfile in $scalarsourcefiles ; do
-    if [ -f $sfile ]; then
-      sfilenew="${sfile%.$src}"
-      sfilename="${sfilenew##*$src/}"
-      cp $sfile $DA_EXPDIR/$tar/$sfilename"."$tar
-    fi
-  done
+scalarsourcefiles=$DA_EXPDIR/$src/"scalarsource"*
+for sfile in $scalarsourcefiles ; do
+  if [ -f $sfile ]; then
+    sfilenew="${sfile%.$src}"
+    sfilename="${sfilenew##*$src/}"
+    cp $sfile $DA_EXPDIR/$tar/$sfilename"."$tar
+  fi
+done
