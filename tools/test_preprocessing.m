e694b80809094ffdc762880d04440771b094c5f4
Daniel Cummins <daniel.cummins17@imperial.ac.uk>
2024-08-29 16:17:57 +0100

    Use example 102 for testing

--- tools/test_preprocessing.m
+++ tools/test_preprocessing.m
@@ -5,7 +5,7 @@ classdef test_preprocessing < matlab.unittest.TestCase
 
         % Test preprocessing.set_nfcts
         function test_set_nfcts(testCase)
-            expmnt = preprocessing('064', '/home/dc2917/Documents/uDALES/experiments/');
+            expmnt = preprocessing(102, '../examples/');
             preprocessing.set_nfcts(expmnt, 666);
             testCase.verifyEqual(expmnt.nfcts, 666);
         end
