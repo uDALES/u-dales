7bad147239cd63c20f151167843b3a4b0a5f392c
Daniel Cummins <daniel.cummins17@imperial.ac.uk>
2024-08-30 15:13:20 +0100

    Add test for preprocessing.addvar

--- tools/test_preprocessing.m
+++ tools/test_preprocessing.m
@@ -3,6 +3,19 @@ classdef test_preprocessing < matlab.unittest.TestCase
 
     methods(Test, TestTags = {'Unit'})
 
+        % Test preprocessing.addvar
+        function test_addvar(testCase)
+            expmnt = preprocessing(102, '../examples/');
+
+            % Add a new variable via addvar, check its value
+            preprocessing.addvar(expmnt, "a_new_variable", 123);
+            testCase.verifyEqual(expmnt.a_new_variable, 123);
+
+            % Try adding the same variable via addvar, check its value
+            preprocessing.addvar(expmnt, "a_new_variable", 456);
+            testCase.verifyNotEqual(expmnt.a_new_variable, 456);
+        end
+
         % Test preprocessing.set_nfcts
         function test_set_nfcts(testCase)
             expmnt = preprocessing(102, '../examples/');
