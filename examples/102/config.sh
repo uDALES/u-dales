16fb8d419d50f9bbe0cbaac387e810ce9832464d
cedwardwilson <c.edward.wilson@gmail.com>
2024-08-30 10:12:59 +0100

    Updating examples to be consistent with uDALES v2.0 format.

---
+++ examples/102/config.sh
@@ -0,0 +1,5 @@
+export DA_EXPDIR=/media/chris/Project3/uDALES2.0/experiments
+export DA_TOOLSDIR=/media/chris/Project3/uDALES2.0/u-dales/tools
+export DA_BUILD=/media/chris/Project3/uDALES2.0/u-dales/build/release/u-dales
+export DA_WORKDIR=/media/chris/Project3/uDALES2.0/outputs
+export NCPU=4
