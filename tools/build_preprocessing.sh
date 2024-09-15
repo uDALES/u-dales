cf2d467f529f86865c005522aad3c6c07cc762b2
Dipanjan Majumdar <dipanjanmajumdar100@gmail.com>
2024-09-15 01:50:07 +0100

    added flexibility to build_preprocessing.sh

--- tools/build_preprocessing.sh
+++ tools/build_preprocessing.sh
@@ -2,9 +2,25 @@
 
 set -e
 
+# Usage: ./tools/build_preprocessing.sh [icl]
+
+if [ ! -d tools ]; then
+    echo "Please run this script from being inside the u-dales folder"
+    exit 1
+fi
+
 cd tools/View3D
 mkdir build
 cd build
+
+system=$1
+if [ $system == "icl" ]
+then
+    module load cmake/3.18.2
+else
+    echo "This configuration is not avalable"
+    exit 1
+fi
 
 cmake ..
 echo "View3D configuration complete."
