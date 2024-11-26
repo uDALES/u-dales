f9efc2de266dae04071e0407b1f9a2ab148a20b4
Dipanjan Majumdar <dipanjanmajumdar100@gmail.com>
2024-11-26 14:23:09 +0000

    Update install_deps.sh

--- .github/scripts/install_deps.sh
+++ .github/scripts/install_deps.sh
@@ -6,7 +6,6 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     sudo apt update
     sudo apt install gfortran libopenmpi-dev openmpi-bin libnetcdf-dev libnetcdff-dev graphviz libfftw3-dev
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-    brew unlink pkg-config@0.29.2
     brew install pkgconf
     brew link --overwrite pkgconf
     brew install netcdf netcdf-fortran open-mpi graphviz fftw
