60ae7578f0bd7e5be294d77313cc0cd4c289bd87
Dipanjan Majumdar <dipanjanmajumdar100@gmail.com>
2025-12-21 20:19:44 +0000

    Avoided redundant dthvdz computation. Removed redundant commentend lines.

--- src/modsubgrid.f90
+++ src/modsubgrid.f90
@@ -325,7 +325,6 @@ contains
              bb = b11*b22 - b12*b12 + b11*b33 - b13*b13 + b22*b33 - b23*b23
 
              ekm(i,j,k) = c_vreman*sqrt( max( (bb/aa), 0.0 ) )   ! Eddy viscosity
-
            end do
          end do
        end do
@@ -342,92 +341,20 @@ contains
              do i = ib,ie
                ip = i+1
 
-               dthvdz(i,j,k) = ( thl0(i,j,kp) - thl0(i,j,km) )/(dzh(kp)+dzh(k))
+               !! No need to calculate dthvdz here as it anyway gets calculated in thermodynamics module in calthv subroutine. Avoid redundant computation here.
+               !dthvdz(i,j,k) = ( thl0(i,j,kp) - thl0(i,j,km) )/(dzh(kp)+dzh(k))
+               
                du0dz = 0.5*( (u0(i,j,kp)+u0(ip,j,kp)) - (u0(i,j,km)+u0(ip,j,km)) )/(dzh(kp)+dzh(k))
                dv0dz = 0.5*( (v0(i,j,kp)+v0(i,jp,kp)) - (v0(i,j,km)+v0(i,jp,km)) )/(dzh(kp)+dzh(k))
                Rig = ( (grav/thl0(i,j,k)) * dthvdz(i,j,k) ) / (du0dz**2 + dv0dz**2 + 1.e-10)     ! Eq. B4
 
                ekm(i,j,k) = ekm(i,j,k) * sqrt( 1.0 - min(max(Rig,0.0),Rigc) / Rigc )             ! Eq. B3
                ! Bouyancy correction applied only for stable stratification Rig > 0
-               ! For Rig >= Rigc, ekm = 0
-               
+               ! For Rig >= Rigc, ekm = 0  
              end do
            end do
          end do
        end if
-
-
-       !! Old implementation of buoyancy correction; not consistent with uDALED v2.0 onwards.
-       !! D. Majumdar and X. Long have decided to replace the following implementation with above.
-       !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-       !  if ((lbuoyancy) .and. (lbuoycorr)) then
-       !    const = prandtli*grav/(thvs*sqrt(2.*3.))
-       !    do k = kb,ke
-       !      kp = k+1
-       !      km = k-1
-       !      do j = jb,je
-       !        jp = j+1
-       !        jm = j-1
-       !        do i = ib,ie        ! aij = du_j / dx_i
-       !          ip = i+1
-       !          im = i-1
-
-       !          a11 = (u0(ip,j,k) - u0(i,j,k)) * dxi
-
-       !          a12 = (v0(ip,jp,k) + v0(ip,j,k) - v0(im,jp,k) - v0(im,j,k) )*dxiq
-
-       !          a13 = (w0(ip,j,kp) + w0(ip,j,k) - w0(im,j,kp) - w0(im,j,k) )*dxiq
-
-       !          a21 = (u0(ip,jp,k) + u0(i,jp,k) - u0(ip,jm,k) - u0(i,jm,k) )*dyiq
-
-       !          a22 = (v0(i,jp,k) - v0(i,j,k)) * dyi
-
-       !          a23 = (w0(i,jp,kp) + w0(i,jp,k) - w0(i,jm,kp) - w0(i,jm,k) )*dyiq
-
-       !          a31 = ( &
-       !                ((u0(ip,j,kp) + u0(i,j,kp))*dzf(k) + (u0(ip,j,k)  + u0(i,j,k)) *dzf(kp)) * dzhi(kp) &
-       !              - ((u0(ip,j,k)  + u0(i,j,k)) *dzf(km) +(u0(ip,j,km) + u0(i,j,km))*dzf(k))  * dzhi(k)  &
-       !                ) &
-       !              * dzfiq(k)
-
-       !          a32 = ( &
-       !                ((v0(i,jp,kp) + v0(i,j,kp))*dzf(k) + (v0(i,jp,k)  + v0(i,j,k)) *dzf(kp)) * dzhi(kp) &
-       !              - ((v0(i,jp,k)  + v0(i,j,k)) *dzf(km) +(v0(i,jp,km) + v0(i,j,km))*dzf(k))  * dzhi(k)  &
-       !                ) &
-       !                *dzfiq(k)
-
-       !          a33 = (w0(i,j,kp) - w0(i,j,k)) * dzfi(k)
-
-       !          aa  = a11*a11 + a21*a21 + a31*a31 + &
-       !                a12*a12 + a22*a22 + a32*a32 + &
-       !                a13*a13 + a23*a23 + a33*a33
-
-       !          b11 = dx2*a11*a11 + dy2*a21*a21 + dzf2(k)*a31*a31
-       !          b22 = dx2*a12*a12 + dy2*a22*a22 + dzf2(k)*a32*a32
-       !          b12 = dx2*a11*a12 + dy2*a21*a22 + dzf2(k)*a31*a32
-       !          b33 = dx2*a13*a13 + dy2*a23*a23 + dzf2(k)*a33*a33
-       !          b13 = dx2*a11*a13 + dy2*a21*a23 + dzf2(k)*a31*a33
-       !          b23 = dx2*a12*a13 + dy2*a22*a23 + dzf2(k)*a32*a33
-       !          bb = b11*b22 - b12*b12 + b11*b33 - b13*b13 + b22*b33 - b23*b23
-
-       !          dthvdz(i,j,k) = (thl0(i,j,k+1)-thl0(i,j,k-1))/(dzh(k+1)+dzh(k))
-       !          if (dthvdz(i,j,k) <= 0) then
-       !            const2=(bb/aa)
-       !          else
-       !            const2=(bb/aa)-(delta(i,k)**4)*dthvdz(i,j,k)*const
-       !          end if
-        
-       !          if (const2 < 0.00000001) then
-       !            ekm(i,j,k) = 0.
-       !          else
-       !            ekm(i,j,k)=c_vreman*sqrt(const2)
-       !          end if    
-       !        end do
-       !      end do
-       !    end do
-       !  end if
-       !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
 
        ekh(:,:,:) = ekm(:,:,:)*prandtli              ! Eddy diffusivity from eddy viscosity, Kh = Km/Pr
        ! ekh(:,:,:) = max(ekh(:,:,:),ekmin)
