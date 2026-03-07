ac648ddf5dd6998eb1cee155f1fab61f34fb262a
Dipanjan Majumdar <dipanjanmajumdar100@gmail.com>
2026-03-07 21:29:28 +0000

    Updated file header.

--- src/out_stats.f90
+++ src/out_stats.f90
@@ -1,3 +1,28 @@
+!> \file out_stats.f90
+!! Writes statistics of various fields to various NetCDF files.
+!>
+!
+!! Inspired from the uDALES v2.2.0 modstatsdump.f90 routine by Tom Grylls, ICL (2016).
+!! \author Dipanjan Majumdar, ICL (2025-2026)
+!! \todo documentation
+!
+! This file is part of uDALES (https://github.com/uDALES/u-dales).
+!
+! uDALES is free software; you can redistribute it and/or modify
+! it under the terms of the GNU General Public License as published by
+! the Free Software Foundation; either version 3 of the License, or
+! (at your option) any later version.
+!
+! uDALES is distributed in the hope that it will be useful,
+! but WITHOUT ANY WARRANTY; without even the implied warranty of
+! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+! GNU General Public License for more details.
+!
+! You should have received a copy of the GNU General Public License
+! along with this program.  If not, see <http://www.gnu.org/licenses/>.
+!
+! Copyright (C) 2016- the uDALES Team, Imperial College London.
+!
 module stats
   use modglobal,  only : cexpnr, ltempeq, lmoist, lchem, nsv, rk3step, &
                          ltdump, lxytdump, lxydump, lytdump, lydump, ltreedump, &
