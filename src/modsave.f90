5cc0c8d29fe3d511e8d10c7fc7309a812a2dd5d5
Dipanjan Majumdar <dipanjanmajumdar100@gmail.com>
2026-03-16 11:41:15 +0000

    Update license headers from DALES to uDALES in modfields and modsave.

--- src/modsave.f90
+++ src/modsave.f90
@@ -1,19 +1,20 @@
 !> \file modsave.f90
 !! Writes restart and data files.
 !>
-!! modsave.f90 writes the restart and data files
-!!  \author Jasper Tomas, June 4th 2015
-!!  \todo documentation
-!!  \par Revision list
 !
-!  This file is part of DALES.
+!! \author Jasper Tomas, June 4th 2015
+!! \par Revision list
+!!   Dipanjan Majumdar, ICL (2025-2026)
+!! \todo documentation
 !
-! DALES is free software; you can redistribute it and/or modify
+! This file is part of uDALES (https://github.com/uDALES/u-dales).
+!
+! uDALES is free software; you can redistribute it and/or modify
 ! it under the terms of the GNU General Public License as published by
 ! the Free Software Foundation; either version 3 of the License, or
 ! (at your option) any later version.
 !
-! DALES is distributed in the hope that it will be useful,
+! uDALES is distributed in the hope that it will be useful,
 ! but WITHOUT ANY WARRANTY; without even the implied warranty of
 ! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ! GNU General Public License for more details.
@@ -21,7 +22,7 @@
 ! You should have received a copy of the GNU General Public License
 ! along with this program.  If not, see <http://www.gnu.org/licenses/>.
 !
-!  Copyright 1993-2009 Delft University of Technology, Wageningen University, Utrecht University, KNMI
+! Copyright (C) 2016- the uDALES Team, Imperial College London.
 !
 module modsave
 
